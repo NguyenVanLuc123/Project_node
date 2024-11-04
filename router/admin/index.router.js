77943431dc1a732d77c82310e21ae3f5b591f199
NguyenVanLucIT05 <alaluclun11032003@gmail.com>
2024-11-05 00:07:19 +0700

    new7

--- router/admin/index.router.js
+++ router/admin/index.router.js
@@ -1,6 +1,6 @@
 const dashboardRouter = require("./dashboard.router.js")
 
-const SystemConfig=require("../../config/system.js")
+const SystemConfig=require(`${__dirname}/../../config/system.js`)
 
 const productsAdminRouter=require("./product.router.js")
 module.exports = (app)=>{
